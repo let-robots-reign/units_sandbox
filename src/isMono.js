1775b2770423477650eca036397723895db82712
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-09-23 21:15:26 +0300

    QA class: tests and realization

--- src/isMono.js
+++ src/isMono.js
@@ -6,5 +6,25 @@
  *  isMono([0, 1, 0]) вернет false
  */
 export function isMono(values) {
+    if (!Array.isArray(values)) {
+        throw new TypeError("invalid params: 'values' is not an Array");
+    }
+    if (typeof values[0] !== 'number') {
+        throw new TypeError("invalid params: 'values' elements are not all numbers");
+    }
 
+    let isIncreasing = true;
+    let isDecreasing = true;
+  
+    for (let i = 0; i < values.length - 1; ++i) {
+      if (values[i] > values[i + 1]) {
+        isIncreasing = false;
+      }
+  
+      if (values[i] < values[i + 1]) {
+        isDecreasing = false;
+      }
+    }
+  
+    return isIncreasing || isDecreasing;
 }
--- src/isMono.test.js
+++ src/isMono.test.js
@@ -9,6 +9,49 @@ describe('test isMono function', () => {
     assert.strictEqual(result, true);
   });
 
-  // Добавьте сюда ваши тесты
-})
+  test('[3, 2, 1]', () => {
+    const result = isMono([3, 2, 1]);
 
+    assert.strictEqual(result, true);
+  });
+
+  test('[1, 100, 1000, -1]', () => {
+    const result = isMono([1, 100, 1000, -1]);
+
+    assert.strictEqual(result, false);
+  });
+
+  test('[0, 0, 0, 0, 0]', () => {
+    const result = isMono([0, 0, 0, 0, 0]);
+
+    assert.strictEqual(result, true);
+  });
+
+  test("['aba', 'abb', 'abc', 'abd']", () => {
+    assert.throws(
+      () => isMono(['aba', 'abb', 'abc', 'abd']), 
+      TypeError,
+    );
+  });
+
+  test('null', () => {
+    assert.throws(
+      () => isMono(null), 
+      TypeError,
+    );
+  });
+
+  test('undefined', () => {
+    assert.throws(
+      function() {isMono(undefined)}, 
+      TypeError,
+    );
+  })
+
+  test("{'a': 10}", () => {
+    assert.throws(
+      () => isMono({ 'a': 10 }), 
+      TypeError,
+    );
+  })
+});
